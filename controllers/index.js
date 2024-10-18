0fc6bd9a35a16e1917383d68c94e82c4c84237a9
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-10-18 20:41:07 -0300

    inicio do projeto oauth

---
+++ controllers/index.js
@@ -0,0 +1,44 @@
+const User = require("../models/user");
+
+exports.showIndex = (req, res, next) => {
+  res.render("index");
+};
+
+exports.showPageSignUp = (req, res, next) => {
+  res.render("signUp");
+};
+
+exports.showMembersPage = (req, res) => {
+  res.render("members");
+};
+
+exports.get404Page = (req, res, next) => {
+  res.status(404).render("404");
+};
+
+exports.signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = new User(username, email, password);
+  try {
+    await user.save();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.redirect("signup");
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne(email, password);
+  try {
+    if (user) {
+      res.redirect("/members");
+    } else {
+      res.render("index");
+    }
+  } catch (err) {
+    console.log(err);
+    res.render("index");
+  }
+};
